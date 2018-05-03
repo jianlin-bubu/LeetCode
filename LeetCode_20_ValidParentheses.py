aeff409bbb5b6b7a640d26783a47c5537bbeff73
bubu <msear.tw@gmail.com>
2018-05-03 21:46:54 +0800

    practice 20

---
+++ LeetCode_20_ValidParentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        remainder = [None]
+        right_dic = {')':'(',']':'[','}':'{'}
+        for i in s:
+            if i in right_dic and right_dic[i] == remainder[-1]:
+                remainder.pop()
+            else:
+                remainder.append(i)
+        return len(remainder) == 0
