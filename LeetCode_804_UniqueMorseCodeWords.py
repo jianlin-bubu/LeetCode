e824a8f934b3dd391bf03158996e8c8bcf150ecd
bubu <msear.tw@gmail.com>
2018-05-07 20:25:44 +0800

    practice 804 version 2

--- LeetCode_804_UniqueMorseCodeWords.py
+++ LeetCode_804_UniqueMorseCodeWords.py
@@ -9,10 +9,24 @@ class Solution(object):
 
         #letter to morse
         letter_to_morse = lambda x: morse_code[ord(x) - ord('a')]
+        """
+        #equivalent
+        def letter_to_morse_def(x):
+            return morse_code[ord(x) - ord('a')]
+        """
         #word to morse
         word_to_morse = lambda word: ''.join([letter_to_morse(x) for x in word])
+        """
+        #equivalent:
+        def word_to_morse_def(word):
+            l = []
+            for x in word:
+                l.append(letter_to_morse(x))
+            return ''.join(l)
+        """
         #all words to morse
         words_to_morse = map(word_to_morse, words)
+        #equivalent: words_to_morse = [word_to_morse(word) for word in words]
         #unique
         return len(set(words_to_morse))
 
